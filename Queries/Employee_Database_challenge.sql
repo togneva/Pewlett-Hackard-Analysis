047aa29e441cd2c86e3fdd5ddc505225bbb9df8e
Tatiana Ogneva <togneva@gmail.com>
2022-01-09 10:47:02 -0800

    Update queires file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,6 +30,28 @@ FROM unique_retirement_titles
 GROUP BY title
 ORDER BY title_count DESC
 
+-- Get employees eligible for mentorship program
+SELECT DISTINCT ON (e.emp_no)
+		e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ttl.title
+INTO mentoship_eligibility
+FROM employees AS e
+JOIN dept_employees AS de
+	ON e.emp_no = de.emp_no
+JOIN titles AS ttl
+	ON e.emp_no = ttl.emp_no
+WHERE de.to_date = '9999-01-01'
+	AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no, ttl.to_date DESC
+
+
+		
+
 
 
 
